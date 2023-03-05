9440d492c280add59174254bcb950db382b6a005
confusedPineapple <isabelxcoding@gmail.com>
2023-03-05 12:33:56 -0800

    finised pingpong game

---
+++ ball.py
@@ -0,0 +1,32 @@
+import random
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color('red')
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        x = self.xcor() + self.x_move
+        y = self.ycor() + self.y_move
+        self.goto(x,y)
+
+    def bounce_y(self):
+        self.y_move  *= -1
+
+    def bounce_x(self):
+        self.x_move  *= -1
+        self.move_speed *= 0.9
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,68 @@
+from turtle import Screen
+from turtle import Turtle
+from scoreboard import Scoreboard
+from ball import Ball
+from paddle import Paddle
+import time
+import winsound
+
+screen = Screen()
+screen.setup(width=1100, height= 600)
+screen.bgcolor('pink')
+screen.title('My PingPong Game <3')
+screen.tracer(0)
+r_paddle = Paddle((500, 0))
+l_paddle = Paddle((-500, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 80
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect Collision with Wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect Collision with r_paddle
+    if ball.distance(r_paddle) < 30 and ball.xcor() > 340 or ball.distance(l_paddle) < 30 and ball.xcor() < -340 :
+        # winsound.Beep(frequency, duration)
+        ball.bounce_x()
+
+
+    # Detect out of bounds ball
+    #Right
+    if ball.xcor() > 600:
+        scoreboard.l_point()
+        ball.goto(0,0)
+        ball.x_move = -10
+        ball.y_move = -10
+        ball.move_speed = 0.1
+        ball.move()
+
+    #Left
+    elif ball.xcor() < -600 :
+        scoreboard.r_point()
+        ball.goto(0,0)
+        ball.x_move = 10
+        ball.y_move = 10
+        ball.move_speed = 0.1
+        ball.move()
+
+
+
+
+
+screen.exitonclick()
